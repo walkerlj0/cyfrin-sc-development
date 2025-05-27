55a10e00f5fb2f987632aa902308182d7183d209
Lindsay jo Walker <lindsay.lintereur@gmail.com>
2025-05-27 12:37:12 -0500

    pushing only what I want

--- contracts/SimpleStorageArray.sol
+++ contracts/SimpleStorageArray.sol
@@ -2,22 +2,32 @@
 pragma solidity 0.8.19; //. 0.8.18 is a stable solidity version
 
 contract SimpleStorage {
-    bool hasFavoriteNumber = false; 
-    string favoriteNumberInText = "-`17";
-    int256 favoriteInt = 17; // default 1
-    address myAddress = 0x1C016C9A15e9Ef085bD0225bc5fa834b114a13e1;
-    bytes32 favoriteBytes32 = "dog"; // 0xbcf2518, the hex of bytes, but can be represented by strings
+    uint256 public myFavoriteNumber; // default value is 0
+    // uint256[] listOfFavoriteNumbers;
+    struct Person{
+        uint256 favoriteNumber;
+        string name;
+    }
+    // Person public myFemaleFriend = Person(17, "Claire");
+    // Person public myMaleFriend = Person({favoriteNumber: 25, name: "Tom"});
 
-    uint256 public favoriteNumber; // default value is 0
-    uint256 public otherNumber;
+    // dynamic array can grow & shrink (no set size)
+    Person[] public listOfPeople;
+
 
     function store(uint256 _favoriteNumber)  public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
         retrieve(); // this will cost gas as it's being called from a gas-calling function
     }
 
     // pure and view don't cost gas/ do anything on-chain. neither can update state, and view can't even read state
     function retrieve() public view returns(uint256){
-        return favoriteNumber;
+        return myFavoriteNumber;
+
     } 
-}
+
+    function addPerson(string memory _name, uint256 _favorieNumber) public{
+        // Person memory newPerson = Person(_favorieNumber, _name);
+        // listOfPeople.push();
+        listOfPeople.push(Person(_favorieNumber, _name)); //Person is created before the push func is executed
+    }
