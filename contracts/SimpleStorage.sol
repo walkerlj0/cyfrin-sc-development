4aa0be4444cae3db6aad62dea022e88f3d923103
walkerlj0 <lindsay.lintereur@gmail.com>
2025-05-27 10:57:47 -0500

    removed other vars

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -2,12 +2,6 @@
 pragma solidity 0.8.19; //. 0.8.18 is a stable solidity version
 
 contract SimpleStorage {
-    bool hasFavoriteNumber = false; 
-    string favoriteNumberInText = "-`17";
-    int256 favoriteInt = 17; // default 1
-    address myAddress = 0x1C016C9A15e9Ef085bD0225bc5fa834b114a13e1;
-    bytes32 favoriteBytes32 = "dog"; // 0xbcf2518, the hex of bytes, but can be represented by strings
-
     uint256 public favoriteNumber; // default value is 0
     uint256 public otherNumber;
 
