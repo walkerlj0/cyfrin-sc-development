94d5719d22c4a8c06b747c4bd0d9103982f90945
Lindsay jo Walker <lindsay.lintereur@gmail.com>
2025-05-28 12:41:56 -0500

    added generted files to .gitignore

--- contracts/SimpleStorageMem.sol
+++ contracts/SimpleStorageMem.sol
@@ -2,6 +2,7 @@
 pragma solidity 0.8.19; //. 0.8.18 is a stable solidity version
 
 contract SimpleStorage {
+    //implicitly storage var, declared outside a func
     uint256 public myFavoriteNumber; // default value is 0
     // uint256[] listOfFavoriteNumbers;
     struct Person{
@@ -20,7 +21,13 @@ contract SimpleStorage {
 
     } 
 
-    // calldata, memory, storage
+    // calldata, memory, storage variables 
+    // The _name var is only accessible when func is called if ver is calldata or memory - temporary
+    // memory data can be changed, calldata only created onced
+    // storage is permanent data that can be modified. Can't make a func var storage
+    //arrays, structs, and types are specialy types, need declarations. A string is actually a type of array, ergo needs type
     function addPerson(string memory _name, uint256 _favorieNumber) public{
+        _name = "Linus";
         listOfPeople.push(Person(_favorieNumber, _name)); //Person is created before the push func is executed
     }
+}
